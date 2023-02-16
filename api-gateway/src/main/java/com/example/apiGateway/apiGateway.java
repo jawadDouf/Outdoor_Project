77f592641c9f7857dbed3e55d74bfbf9f71fa45a
jawadDouf <jdoufare2001@gmail.com>
2023-02-16 14:54:39 +0100

    Create the routing for the product service

---
+++ api-gateway/src/main/java/com/example/apiGateway/apiGateway.java
@@ -0,0 +1,15 @@
+package com.example.apiGateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class apiGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(apiGateway.class, args);
+    }
+}
